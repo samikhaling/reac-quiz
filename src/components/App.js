0172be1350cec2ddf9098d960a9ff9a64c442131
Samir Rai <samikhaling14@gmail.com>
2023-09-26 22:15:22 -0400

    Added time interval to quizz

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,10 @@ import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
 import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
   questions:[],
@@ -16,7 +20,8 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
-  highscore: 0
+  highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state,action) {
@@ -35,7 +40,8 @@ function reducer(state,action) {
     case "start":
       return {
         ...state,
-        status: "active"
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       };
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -55,13 +61,22 @@ function reducer(state,action) {
           highscore:
             state.points > state.highscore? state.points : state.highscore,
         };
+    case "restart":
+      return {...initialState,question: state.questions, status: "ready"};
+
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{questions, status, index, answer, points , highscore}, dispatch] = useReducer(reducer,initialState);
+  const [{questions, status, index, answer, points , highscore , secondsRemaining}, dispatch] = useReducer(reducer,initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce((prev,cur)=>prev + cur.points,0);
@@ -98,17 +113,26 @@ export default function App() {
               answer={answer}
               
             />
-            <NextButton 
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining}/>
+              <NextButton 
               dispatch={dispatch} 
               answer={answer}
               numQuestions={numQuestions}
               index={index}
             />
+            </Footer>
+            
         </>
         
       )}
       {status === "finished" && (
-        <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} highscore={highscore} />
+        <FinishScreen 
+          points={points} 
+          maxPossiblePoints={maxPossiblePoints} 
+          highscore={highscore} 
+          dispatch={dispatch}
+        />
       )}
       </Main>
     </div>
--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,4 +1,4 @@
-function FinishScreen({ points, maxPossiblePoints, highscore }) {
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
   const percentage = (points / maxPossiblePoints) * 100;
   let emoji;
   if (percentage === 100) emoji = "🥇";
@@ -12,6 +12,12 @@ function FinishScreen({ points, maxPossiblePoints, highscore }) {
         <span>{emoji}</span>You scored <strong>{points}</strong> out of {maxPossiblePoints} ({Math.ceil(percentage)}%)
       </p>
       <p className="highscore">(Highscore = {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart quiz
+      </button>
     </>
   );
 }
